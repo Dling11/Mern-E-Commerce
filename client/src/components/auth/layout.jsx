525c56c18631892a8cb7b244c91321a7d4bf8269
RowellJay19 <row.rodriguez@awesomecx.com>
2025-02-05 16:45:45 +0800

    - added name

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -8,7 +8,7 @@ function AuthLayout() {
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
           <h1 className="text-4xl font-extrabold tracking-tight">
-            Welcome to HAKING SHOP E-COMMERCE : {")"}
+            Welcome to HAKING SHOP E-COMMERCE OF ROWELL : {")"}
           </h1>
           <div className="relative w-full">
             <img
